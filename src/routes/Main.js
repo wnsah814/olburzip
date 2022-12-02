de025bee7e4a8dc7a71c4c1220e9ac210236ea6b
wnsah814 <sjmskm@gmail.com>
2022-12-03 03:40:40 +0900

    change logo img to svg file

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from "./Main.module.css";
-import logo from "../asset/img/header_logo.png";
+import logo from "asset/img/logo.svg";
 const Main = () => {
-  return (
-    <div className={styles.wrapper}>
-      <img className={styles.logoImg} src={logo} />
-      <h1>우주최강 율동패 어리버리</h1>
-      <br />
-      <Link to="apply">
-        <button className={styles.applyBtn}>지원하기</button>
-      </Link>
-    </div>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <img className={styles.logoImg} src={logo} alt="logo" />
+            <h1>우주최강 율동패 어리버리</h1>
+            <br />
+            <Link to="apply">
+                <button className={styles.applyBtn}>지원하기</button>
+            </Link>
+        </div>
+    );
 };
 export default Main;
