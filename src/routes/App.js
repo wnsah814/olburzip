10569c7153d139de4a62256849f3c01ed510e224
wnsah814 <sjmskm@gmail.com>
2022-12-02 14:58:06 +0900

    color chng

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,12 +2,14 @@ import "asset/css/style.css";
 import Header from "components/Header";
 import { BrowserRouter as Router } from "react-router-dom";
 import Contents from "components/Contents";
+import Footer from "components/Footer";
 
 function App() {
     return (
         <Router>
             <Header />
             <Contents />
+            <Footer />
         </Router>
     );
 }
