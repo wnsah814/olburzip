76d6219f8256b8490211415f384a43fa04c45bc7
성준모 <sjmskm@gmail.com>
2023-01-23 19:18:21 +0900

    design: Recover original designs and add fonts

--- src/components/ToastUI/Viewer.tsx
+++ src/components/ToastUI/Viewer.tsx
@@ -1,34 +1,41 @@
 import { Viewer } from "@toast-ui/react-editor";
-
+import styles from "@/styles/Viewer.module.css";
 interface ViewerProps {
     content: any;
 }
 
 const PostViewer = ({ content }: ViewerProps) => {
     return (
-        <Viewer
-            initialValue={content}
-            customHTMLRenderer={{
-                htmlBlock: {
-                    iframe(node: any) {
-                        return [
-                            {
-                                type: "openTag",
-                                tagName: "iframe",
-                                outerNewLine: true,
-                                attributes: node.attrs,
+        <>
+            <div className={styles.blog}>
+                <Viewer
+                    initialValue={content}
+                    customHTMLRenderer={{
+                        htmlBlock: {
+                            iframe(node: any) {
+                                return [
+                                    {
+                                        type: "openTag",
+                                        tagName: "iframe",
+                                        outerNewLine: true,
+                                        attributes: node.attrs,
+                                    },
+                                    {
+                                        type: "html",
+                                        content: node.childrenHTML,
+                                    },
+                                    {
+                                        type: "closeTag",
+                                        tagName: "iframe",
+                                        outerNewLine: false,
+                                    },
+                                ];
                             },
-                            { type: "html", content: node.childrenHTML },
-                            {
-                                type: "closeTag",
-                                tagName: "iframe",
-                                outerNewLine: false,
-                            },
-                        ];
-                    },
-                },
-            }}
-        ></Viewer>
+                        },
+                    }}
+                ></Viewer>
+            </div>
+        </>
     );
 };
 
