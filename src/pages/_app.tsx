3f4586e9565d3ffecdcf7512853db18f3f0ad4b5
성준모 <sjmskm@gmail.com>
2023-01-25 01:18:54 +0900

    feat: import @vercel/analytics
    able to see visitors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Layout>
             <Component {...pageProps} />
+            <Analytics />
         </Layout>
     );
 }
